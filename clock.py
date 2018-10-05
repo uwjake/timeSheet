0dba4aa5127275ef14e76317c87dbeba072944b5
Jake <hnujzh@hotmail.com>
2018-10-05 00:34:39 -0700

    use csv instead

--- clock.py
+++ clock.py
@@ -13,22 +13,29 @@ def get_last_day_of_month(year, month):
     return datetime.date (year, month+1, 1) - datetime.timedelta (days = 1)
 
 def save(df):
-    writer = pd.ExcelWriter('timesheet.xlsx')
-    df.to_excel(writer,'Sheet1')
-    writer.save()
+    # writer = pd.ExcelWriter('timesheet.xlsx')
+    # df.to_csv("timesheet.csv", sep=',', index=False, encoding='utf-8')
+    # writer.save()
     print(df.head())
 
 def clock_in():
-    df = pd.read_excel("timesheet.xlsx")
+    df = pd.read_csv("timesheet.csv")
+    print(df)
     df.loc[-1] = [today_date, time_now, 0.0, 0.0]  # adding a row
     df.index = df.index + 1  # shifting index
     df.sort_index(inplace=True)
     return df
 
+def read_strptime(str):
+    return datetime.strptime(str, '%Y-%m-%d %H:%M:%S.%f')
+
 def clock_out():
-    df = pd.read_excel("timesheet.xlsx")
+    df = pd.read_csv("timesheet.csv")
     df.at[0,'Out'] = time_now
-    df.loc[0,'Hour'] = round((df.loc[0]['Out'] - df.loc[0]['In']).total_seconds() / 3600, 2)
+    # Out = read_strptime(df.loc[0]['Out'])
+    In = read_strptime(df.loc[0]['In'])
+
+    df.loc[0,'Hour'] = round((time_now - In).total_seconds() / 3600, 2)
     if len(df[df["Date"] == today_date]) >1:
         print('Multiple entry for today')
         print('*'*40)
--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import pandas as pd
+from datetime import datetime
 
-df = pd.read_excel("timesheet.xlsx")
+df = pd.read_csv("timesheet.csv")
+# df = pd.read_excel("timesheet.xlsx")
+
 
 print(df)
 
 print(df.loc[0,'In'])
 print(df.loc[0,'Out'])
+Out = datetime.strptime(df.loc[1]['Out'], '%Y-%m-%d %H:%M:%S.%f')
+In = datetime.strptime(df.loc[1]['In'], '%Y-%m-%d %H:%M:%S.%f')
+print(round((Out - In).total_seconds() / 3600, 2))
 
-print(round((df.loc[1]['Out'] - df.loc[1]['In']).total_seconds() / 3600, 2))
-
-df.to_csv("timesheet.csv", sep=',', encoding='utf-8')
+# df.to_csv("timesheet.csv", sep=',', index=False, encoding='utf-8')
